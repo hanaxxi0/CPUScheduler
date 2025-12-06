d87e19860bae83c16fa83c4602320485499f06f3
Hana Alharbi <iihana11x@gmail.com>
2025-12-06 23:34:55 +0300

    ة

--- cpu-scheduler/vite.config.js
+++ cpu-scheduler/vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: ضع اسم الريبو كما يظهر في رابط GitHub Pages
+  
   base: '/CPUScheduler/',
 })
 
